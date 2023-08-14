a2c3b664d0d520857f3863f907a346570dac0baf
satendra kushwaha <satendra@nimapinfotech.com>
2023-08-14 18:10:34 +0530

    todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@ Injectable({providedIn:"root"})
 export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+  userData:any;
+
+  createData(data:any): void {
+    console.log(data)
+    const headers = { 'content-type': 'application/json'}  
+    this.http.post<any>("http://localhost:3000/add",data)
+}
+
+  getData(): void {
+    this.http.get("http://localhost:3000/user").subscribe(
+        (userData: any) => {
+            // Handle the retrieved user data here
+            this.userData=userData;
+        },
+        (error: any) => {
+            console.error('Error fetching user data:', error);
+        }
+    );
+}
+
+  name:any
+  data=0
+  updateChild(){
+    this.data=Math.random()*1000
+  }
   title = 'my-app';
+  lists:any[] =[]
+  c=0
+  removeTask(id:any) {
+       this.lists=this.lists.filter(list => list.id!=id)
+  }
+
+  addTask(data:string){
+    this.c=this.c+1
+    this.lists.push({id:this.c,task:data})
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChildComponent } from './child/child.component';
+import { LoginComponent } from './login/login.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
